02e5f415b7dfe2b73c4caa5f7ebacc9f2fd8bc26
SparshSuman2005 <sparsh.storage1@gmail.com>
2026-03-29 21:45:28 +0530

    Created FinanceTracker class with ArrayList

---
+++ FinanceTracker.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class FinanceTracker {
+    private ArrayList<Transaction> transactions;
+
+    public FinanceTracker() {
+        this.transactions = new ArrayList<>();
+    }
+
+    // future methods: addTransaction, getBalance, getHistory, etc.
+}
