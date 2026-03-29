988dfed18ce5172e2d4d994ad3c222fe28e71c20
SparshSuman2005 <sparsh.storage1@gmail.com>
2026-03-30 01:13:45 +0530

    Added menu display print statements

--- Main.java
+++ Main.java
@@ -5,7 +5,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            // Program loop - add functionality here
+            System.out.println("\n=== Finance Tracker ===");
+            System.out.println("1. Add Transaction");
+            System.out.println("2. View Balance");
+            System.out.println("3. View History");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
         }
     }
 }
