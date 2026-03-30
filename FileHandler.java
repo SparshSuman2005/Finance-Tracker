d436d908e25e0ab6f75abb32646e38e587d6d487
SparshSuman2005 <sparsh.storage1@gmail.com>
2026-03-30 13:36:09 +0530

    Created FileHandler class skeleton

---
+++ FileHandler.java
@@ -0,0 +1,8 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileHandler {
+    public static final String FILENAME = "finance_data.txt";
+}
