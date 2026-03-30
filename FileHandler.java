48a0f6fd141592c7d9278f5fccece7fff18f931d
SparshSuman2005 <sparsh.storage1@gmail.com>
2026-03-30 13:38:49 +0530

    Implemented writeToFile method

--- FileHandler.java
+++ FileHandler.java
@@ -1,8 +1,23 @@
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class FileHandler {
     public static final String FILENAME = "finance_data.txt";
+
+    public static void saveTransactions(List<Transaction> list) {
+        try (PrintWriter writer = new PrintWriter(new FileOutputStream(FILENAME))) {
+            for (Transaction t : list) {
+                writer.printf("%d,%s,%s,%s,%s%n",
+                        t.getId(),
+                        t.getAmount(),
+                        t.getCategory(),
+                        t.getDate(),
+                        t.getType());
+            }
+        } catch (IOException e) {
+            System.err.println("Error saving transactions: " + e.getMessage());
+        }
+    }
 }
